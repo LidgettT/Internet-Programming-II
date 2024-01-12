7c68c22feefead49c23d5c7b5ece78d91ab854d3
Trayven Lidgett <156277666+LidgettT@users.noreply.github.com>
2024-01-12 16:10:09 -0600

    New pet variables

---
+++ Portfolio Start/Portfolio.js
@@ -0,0 +1,10 @@
+//Trayven Lidgett 1/12/2024
+console.log()
+
+var pets = 3
+
+var petFood = pets * 2
+
+var pet = prompt("What is your favorite pet: ")
+
+console.log("The pet variable is equal to: " + pet)
