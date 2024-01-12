86e9882889b5889b11eee938799846e1574e24b8
Trayven Lidgett <156277666+LidgettT@users.noreply.github.com>
2024-01-12 16:13:28 -0600

    Attaching Portfolio.js to Portfolio.html

--- Portfolio Start/Portfolio.js
+++ Portfolio Start/Portfolio.js
@@ -1,10 +1,10 @@
 //Trayven Lidgett 1/12/2024
-console.log()
+console.log("pets");
 
-var pets = 3
+var pets = 3;
 
-var petFood = pets * 2
+var petFood = pets * 2;
 
-var pet = prompt("What is your favorite pet: ")
+var pet = prompt("What is your favorite pet: ");
 
-console.log("The pet variable is equal to: " + pet)
+console.log("The pet variable is equal to: " + pet);
