dedf9dcb2a06b8929ee26d20cd8e5391514e59ce
Trayven Lidgett <156277666+LidgettT@users.noreply.github.com>
2024-02-18 02:06:04 -0600

    Unit1 interest changes

--- Unit 1 Project/Unit1.js
+++ Unit 1 Project/Unit1.js
@@ -1,11 +1,26 @@
-var cake = true;
+var payment = true;
 
-while (cake == true)
+var amount = [];
+
+//4% interest on car payment
+var modNumber = function (number)
 {
-    var input= prompt("Name multiple cakes or type 0 to stop");
+    return number * .04;
+}
+
+while (payment == true)
+{
+    var input= prompt("Whats your car payment or type 0 to stop");
     if ( input == 0)
     {
         break;
     }
-    console.log(input);
+    amount[amount.length] = input;
+
+    console.log(amount);
+
+    for (var i in amount)
+    {
+        amount[i] = modNumber(amount[i]);
+    }
 }
